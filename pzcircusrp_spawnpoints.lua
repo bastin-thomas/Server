e63fae560b08ec53abf2d0eac33ec6cc41482076
Thomas Bastin <thomasbastin366@gmail.com>
2026-03-24 15:37:48 +0100

    Add PZ Circus Server

---
+++ pzcircusrp_spawnpoints.lua
@@ -0,0 +1,7 @@
+function SpawnPoints()
+	return {
+		unemployed = {
+			{ posX = 16510, posY = 12312, posZ = 0 },
+		},
+	}
+end
---
+++ pzcircusrp_spawnregions.lua
@@ -0,0 +1,13 @@
+function SpawnRegions()
+	return {
+		{ name = "BlackpineCounty", file = "media/maps/BlackpineCounty/spawnpoints.lua" },
+		{ name = "Camden County B42", file = "media/maps/Camden County B42/spawnpoints.lua" },
+		{ name = "Raven Creek B42", file = "media/maps/Raven Creek B42/spawnpoints.lua" },
+		{ name = "Camps de survivants", serverfile = "pzcircusrp_spawnpoints.lua" },
+		{ name = "Echo Creek KY", file = "media/maps/Echo Creek, KY/spawnpoints.lua" },
+		{ name = "Muldraugh KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Riverside KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Rosewood KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "West Point KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+	}
+end
