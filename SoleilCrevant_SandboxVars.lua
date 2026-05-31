f4dec6b5e3018bdb487cbf5aa83f7b2ca50b05d9
Thomas Bastin <thomasbastin366@gmail.com>
2026-05-31 19:20:48 +0200

    map all known

--- SoleilCrevant_SandboxVars.lua
+++ SoleilCrevant_SandboxVars.lua
@@ -779,7 +779,7 @@ SandboxVars = {
         -- If enabled, the world map can be accessed.
         AllowWorldMap = true,
         -- If enabled, the world map will be completely filled in on starting the game.
-        MapAllKnown = false,
+        MapAllKnown = true,
         -- If enabled, maps can't be read unless there's a source of light available.
         MapNeedsLight = true,
     },
