240b4ff274d04b717304f3834ca751f8e8dfe0a0
Thomas Bastin <thomasbastin366@gmail.com>
2026-07-31 01:00:21 +0200

    Add Back old regions

--- SoleilCrevant_spawnregions.lua
+++ SoleilCrevant_spawnregions.lua
@@ -1,10 +1,10 @@
 function SpawnRegions()
 	return {
-		--{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
 		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
-		--{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
-		--{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
 		-- Uncomment the line below to add a custom spawnpoint for this server.
-		--{ name = "Spawn Communs", serverfile = "SoleilCrevant_spawnpoints.lua" },
+--		{ name = "Twiggy's Bar", serverfile = "SoleilCrevant_spawnpoints.lua" },
 	}
 end
